c8fcbd90b606c52349c72bc4fc0ba7e4a8590f6a
wanghublog <wjamesh@qq.com>
2019-11-23 21:30:24 +0800

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("hello world!");
+        ArrayList l = new ArrayList();
+    }
+    public void test() {
+
+        //你好
+    }
+}
